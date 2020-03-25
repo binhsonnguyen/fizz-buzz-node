9c8cb8989771fe8fec9eb1c0c04be92c0e3858ea
edward <son@binhsonnguyen.com>
2020-03-25 19:10:55 +0700

    add fizzbuzz test

--- test/fizzbuzz-test.js
+++ test/fizzbuzz-test.js
@@ -1,8 +1,10 @@
-var assert = require('assert')
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1)
+import fizzBuzz from '../src/fizzbuzz'
+
+const assert = require('assert')
+describe('FizzBuzz', function () {
+  describe('answer', function () {
+    it('should be exists', function () {
+      assert(!!fizzBuzz)
     })
   })
 })
