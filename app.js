36c3f5c400c3317d78ebd0414212302de7996c2c
edward <son@binhsonnguyen.com>
2020-03-25 19:31:44 +0700

    return answer for fizzbuzz questions

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+import fizzBuzz from './src/fizzbuzz'
+
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.use('/fizzbuzz/:number', (req, res) => {
-  res.send(req.params.number)
+  const number = req.params.number
+  const answer = fizzBuzz.answer(number)
+  res.send(answer)
 })
 
 app.use('/', (req, res) => res.send('Hello World!'))
