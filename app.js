ea2d5daff746df130e53b5ebaf02e9a263e615c8
edward <son@binhsonnguyen.com>
2020-03-25 19:27:17 +0700

    get number param from fizzbuzz question

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use('/fizzbuzz/:number', (req, res) => {
+  res.send(req.params.number)
+})
+
 app.use('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
